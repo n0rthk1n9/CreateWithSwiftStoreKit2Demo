b06bcec7a1bd3e391620c270e2649f5c324b5933
Jan Armbrust <jan.armbrust@googlemail.com>
2025-06-16 15:10:12 +0200

    Initial commit

---
+++ CreateWithSwiftStoreKit2DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateWithSwiftStoreKit2DemoApp.swift
+//  CreateWithSwiftStoreKit2Demo
+//
+//  Created by Jan Armbrust on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CreateWithSwiftStoreKit2DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
